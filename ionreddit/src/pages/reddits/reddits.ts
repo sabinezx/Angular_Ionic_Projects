aa6a72cf931e3c02edb0c901dceab3a6e9d06691
sabinezx <sabine_zx@yahoo.com>
2017-02-28 15:53:16 +0200

    first commit

---
+++ ionreddit/src/pages/reddits/reddits.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {RedditService} from '../../app/services/reddit.service';
+import {DetailsPage} from '../details/details';
+
+@Component({
+  selector: 'reddits',
+  templateUrl: 'reddits.html'
+})
+export class RedditsPage {
+items: any;
+category:any;
+limit:any;
+
+  constructor(public navCtrl: NavController, private redditservice:RedditService) {
+this.getDefaults();
+  }
+
+ngOnInit(){
+ this.getPosts(this.category, this.limit);
+}
+
+getDefaults(){
+  if(localStorage.getItem('category')!=null){
+this.category = localStorage.getItem('category');
+  }else {
+    this.category = 'sports';   
+  }  
+
+   if(localStorage.getItem('limit')!=null){
+this.limit = localStorage.getItem('limit');
+  }else {
+    this.limit = 10;   
+  }  
+}
+getPosts(category, limit){
+
+  this.redditservice.getPosts(category, limit).subscribe(response => {this.items = response.data.children});
+}
+
+viewItem(item){
+  this.navCtrl.push(DetailsPage, {
+    item:item
+  });
+}
+
+changeCategory(category){
+ this.getPosts(this.category, this.limit);
+}
+}
+
